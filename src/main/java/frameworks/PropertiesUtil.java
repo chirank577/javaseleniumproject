5328407419430e662758081e301ae5d68e664964
chiranjivi kumar singh <chirank577@gmail.com>
2025-06-04 11:35:21 -0500

    dicussed about properties Util

---
+++ src/main/java/frameworks/PropertiesUtil.java
@@ -0,0 +1,52 @@
+package frameworks;
+
+import com.mysql.cj.util.DnsSrv;
+import lombok.SneakyThrows;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.PropertiesConfigurationLayout;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class PropertiesUtil {
+
+    private InputStreamReader isr;
+    private PropertiesConfiguration propertiesConfiguration;
+    private PropertiesConfigurationLayout propertiesConfigurationLayout;
+
+    @SneakyThrows
+    public PropertiesUtil()
+    {
+        //inputstream reader will read the complete data from the properties file
+        isr=new InputStreamReader(new FileInputStream("Config.properties"));
+        propertiesConfiguration=new PropertiesConfiguration();
+        propertiesConfigurationLayout=new PropertiesConfigurationLayout();
+
+        //It facilitates the reading of the properties file
+        //By transferring the data in InputStreamReader to PropertiesConfiguration
+        //In the propertiesConfiguration object, data will be stored in the form of key-value pairs
+        propertiesConfigurationLayout.load(propertiesConfiguration,isr);
+    }
+
+    private String getProperty(String key)
+    {
+        return propertiesConfiguration.getString(key,key+"not found");
+
+    }
+
+    public String getBrowser()
+    {
+        return getProperty("Browser");
+    }
+    public String getUserName()
+    {
+        return getProperty("UserName");
+    }
+
+    public static void main(String[] args){
+        PropertiesUtil propertiesUtil=new PropertiesUtil();
+        System.out.println(propertiesUtil.getBrowser());
+        System.out.println(propertiesUtil.getUserName());
+    }
+
+}
